9bc73ed3f6663b858763cbf1da0ec04506753089
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2023-05-30 16:09:31 +0300

    Update index.ts

--- pages/api/sms/index.ts
+++ pages/api/sms/index.ts
@@ -21,9 +21,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
