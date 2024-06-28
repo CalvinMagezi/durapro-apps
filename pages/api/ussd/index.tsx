2f2a36dfafbc60822c188049f2518af5ed3ab09f
Calvin Magezi <calvinmagezi@outlook.com>
2024-06-28 16:23:27 +0300

    USSD adjustment

--- pages/api/ussd/index.tsx
+++ pages/api/ussd/index.tsx
@@ -26,7 +26,7 @@ export default async function handler(
             banList?.filter((ban) => ban?.phone_number == phoneNumber).length >
             0
           ) {
-            response = `END You have been banned from using this service. Please contact support for assistance.`;
+            response = `END We apologize, but your access to this service has been restricted. Please contact our support team for assistance.`;
             return res.send(response);
           }
         }
@@ -49,8 +49,24 @@ export default async function handler(
           });
         }
 
-        response = `CON Welcome to Quickset Cashback \n
-        Please Enter The Code Below:`;
+        // Check the number of redeemed codes for this user
+        const { data: redeemedCodes, error: redeemedCodesError } =
+          await supabase
+            .from("cashback_codes")
+            .select("*")
+            .eq("redeemed_by", phoneNumber);
+
+        if (redeemedCodesError) {
+          console.log(redeemedCodesError);
+        }
+
+        if (redeemedCodes && redeemedCodes.length > 30) {
+          response = `END Thank you for your continued use of our service. You have redeemed more than 30 codes. We invite you to visit https://quickset-apps.vercel.app for an enhanced experience.`;
+          return res.send(response);
+        }
+
+        response = `CON Welcome to Quickset Cashback! \n
+        We appreciate your participation. Please enter your code below:`;
       } else {
         const code = text;
         const { data: user, error: userFetchError } = await supabase
@@ -76,14 +92,14 @@ export default async function handler(
           if (user?.wrong_code_count >= 5) {
             await supabase.from("ban_list").insert({
               phone_number: phoneNumber,
-              reason: "Invalid code entry.",
+              reason: "Multiple invalid code entries.",
             });
 
-            response = `END You have attempted to redeem too many incorrect codes. Please contact support for assistance.`;
+            response = `END We're sorry, but you've reached the maximum number of incorrect code attempts. Please contact our support team for further assistance.`;
           } else {
-            response = `END Please ensure you've entered a valid code. You have ${
+            response = `END We apologize, but the code you entered appears to be invalid. You have ${
               5 - user?.wrong_code_count
-            } invalid attempts left.`;
+            } attempts remaining. Please try again.`;
           }
         } else {
           if (data && data[0].redeemed === true) {
@@ -97,14 +113,14 @@ export default async function handler(
             if (user?.wrong_code_count >= 5) {
               await supabase.from("ban_list").insert({
                 phone_number: phoneNumber,
-                reason: "Attempting to redeem a redeemed code.",
+                reason: "Multiple attempts to redeem already redeemed codes.",
               });
 
-              response = `END You have attempted to redeem too many incorrect codes. Please contact support for assistance.`;
+              response = `END We're sorry, but you've reached the maximum number of incorrect code attempts. Please contact our support team for further assistance.`;
             } else {
-              response = `END This code has already been redeemed. You have ${
+              response = `END We apologize, but this code has already been redeemed. You have ${
                 5 - user?.wrong_code_count
-              } attempts left.`;
+              } attempts remaining. Please try a different code.`;
             }
           } else {
             const { error } = await supabase
@@ -127,23 +143,27 @@ export default async function handler(
             ) {
               await transporter.sendMail({
                 ...mailOptions,
-                subject: `User ${phoneNumber} is ready to be paid`,
-                text: `User ${phoneNumber} is ready to be paid`,
-                html: `<h1>User ready to be paid</h1><p>The user with phone number: ${phoneNumber} has redeemed more than 10 codes that haven't been paid for.</p>`,
+                subject: `User ${phoneNumber} is eligible for payment`,
+                text: `User ${phoneNumber} is eligible for payment`,
+                html: `<h1>User eligible for payment</h1><p>The user with phone number: ${phoneNumber} has redeemed more than 10 codes that haven't been paid for.</p>`,
               });
             }
 
             if (error) {
-              response = `END Oops, something went wrong. Please try again`;
+              response = `END We apologize, but an error occurred while processing your request. Please try again later.`;
             } else {
-              response = `END Successfully redeemed code ${code}`;
+              if (codes && codes.length > 30) {
+                response = `END Thank you! Your code ${code} has been successfully redeemed. You have now redeemed more than 30 codes. We invite you to visit https://quickset-apps.vercel.app for an enhanced experience.`;
+              } else {
+                response = `END Thank you! Your code ${code} has been successfully redeemed.`;
+              }
             }
           }
         }
       }
     } catch (error) {
       console.log(error);
-      response = `END Oops, something went wrong. Please try again`;
+      response = `END We apologize, but an error occurred while processing your request. Please try again later.`;
     }
 
     console.log(response);
